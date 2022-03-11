7ab5769842020f3cc03176cca8f0f87e374ad225
Jubayer Hosen <jubayerhosain9838@gmail.com>
2022-03-12 00:13:13 +0800

    all function updated

--- js/app.js
+++ js/app.js
@@ -34,20 +34,23 @@ const switchTab = (id) => {
     if (id === "posts") {
         document.getElementById( "posts" ).style.display = "grid";
         document.getElementById( "liked" ).style.display = "none";
-        document.getElementById( "reported" ).style.display = "none";
+        document.getElementById("reported").style.display = "none";
+        document.getElementById('about-javaScript').style.display = 'block';
     } else if (id === "liked") {
         document.getElementById( "liked" ).style.display = "block";
         document.getElementById( "posts" ).style.display = "none";
-        document.getElementById( "reported" ).style.display = "none";
+        document.getElementById("reported").style.display = "none";
+        document.getElementById('about-javaScript').style.display = 'none';
         displayLikedPosts();
     } else {
         document.getElementById( "reported" ).style.display = "block";
         document.getElementById( "posts" ).style.display = "none";
-        document.getElementById( "liked" ).style.display = "none";
+        document.getElementById("liked").style.display = "none";
+        document.getElementById('about-javaScript').style.display = 'none';
 
         displayReportedPosts();
   }
-  document.getElementById('about-javaScript').style.display = 'none';
+  
 };
 
 const createPost = (post) => {
